bab540e5a0a73dcefd26aac6fa6bd5da399615a8
min <myun02302@gmail.com>
2023-01-14 13:26:42 +0900

    #22-이용 약관 등록 기능 구현

---
+++ src/main/java/com/fastcampus/loan/dto/TermsDTO.java
@@ -0,0 +1,41 @@
+package com.fastcampus.loan.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TermsDTO implements Serializable {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Request {
+
+        private String name;
+
+        private String termsDetailUrl;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Response {
+
+        private Long termsId;
+
+        private String name;
+
+        private String termsDetailUrl;
+
+        private LocalDateTime createAt;
+
+        private LocalDateTime updatedAt;
+
+    }
+}
