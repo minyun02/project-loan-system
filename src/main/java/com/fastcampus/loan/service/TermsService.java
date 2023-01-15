6e911beb0773190d2996be748bd190afa4c101c5
min <myun02302@gmail.com>
2023-01-15 16:53:01 +0900

    #24-이용약관 조회 기능 구현

--- src/main/java/com/fastcampus/loan/service/TermsService.java
+++ src/main/java/com/fastcampus/loan/service/TermsService.java
@@ -2,7 +2,11 @@ package com.fastcampus.loan.service;
 
 import com.fastcampus.loan.dto.TermsDTO.*;
 
+import java.util.List;
+
 public interface TermsService {
 
     Response create(Request request);
+
+    List<Response> getAll();
 }
